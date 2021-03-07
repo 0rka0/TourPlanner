7e199e05f2c753b88eab5ad57ddbb7b806a0d403
David Friedwagner <david.fw3004@gmail.com>
2021-03-07 15:07:36 +0100

    input can be read and button can be executed

---
+++ TourPlanner/TourPlanner/ExecuteSearch.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System;
+using System.Windows.Input;
+
+namespace TourPlanner
+{
+    class ExecuteSearch : ICommand
+    {
+        private readonly ViewModel _viewModel;
+
+        public ExecuteSearch(ViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            _viewModel.PropertyChanged += (sender, args) =>
+            {
+                Debug.Print("cmd: recv prop changed");
+                if (args.PropertyName == "Input")
+                {
+                    Debug.Print("cmd: recv prop changed: Input");
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            Debug.Print("cmd: can exec");
+            return !string.IsNullOrWhiteSpace(_viewModel.Input);
+        }
+
+        public void Execute(object? paramter)
+        {
+            Debug.Print("cmd: exec");
+            _viewModel.Output = "filter: " + _viewModel.Input;
+            TmpFilter(_viewModel.Input);
+            _viewModel.Input = string.Empty;
+        }
+
+        public void TmpFilter(string filter)
+        {
+            Debug.Print("fitler for " + filter);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
---
+++ TourPlanner/TourPlanner/ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace TourPlanner
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string _output = "";
+        private string _input;
+
+        public string Input
+        {
+            get
+            {
+                Debug.Print("reading input");
+                return _input;
+            }
+            set
+            {
+                Debug.Print("writing input");
+                if (Input != value)
+                {
+                    Debug.Print("setting input value");
+                    _input = value;
+
+                    Debug.Print("firing propertyChanged: input");
+                    OnPropertyChanged(nameof(Input));
+                }
+            }
+        }
+
+        public string Output
+        {
+            get
+            {
+                Debug.Print("reading output");
+                return _output;
+            }
+            set
+            {
+                Debug.Print("writing output");
+                if(_output != value)
+                {
+                    Debug.Print("setting output value");
+                    _output = value;
+
+                    Debug.Print("firing propertyChanged: output");
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand ExecuteSearch { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+            this.ExecuteSearch = new ExecuteSearch(this);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Print("prop changed: " + propertyName);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
